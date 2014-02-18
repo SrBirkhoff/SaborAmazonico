2601ec5cef8e822b7e48edd783edc48345b73f59
Suellen Santana <suellen.eng@gmail.com>
2014-02-18 15:31:13 -0400

    still adding menu

---
+++ src/br/com/saboramazonico/activity/TabsPagerAdapter.java
@@ -0,0 +1,33 @@
+package br.com.saboramazonico.activity;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+	public TabsPagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+
+	@Override
+	public Fragment getItem(int index) {
+
+		switch (index) {
+		case 0:
+			return new CategoriasFragment();
+		case 1:
+			return new HomeFragment();
+		case 2:
+			return new FavoritosFragment();
+		}
+
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		return 3;
+	}
+
+}
