33124c482f090c3e54645f10b59ac863636e696b
Suellen Santana <suellen.eng@gmail.com>
2014-02-18 15:26:03 -0400

    adding Menu

--- gen/br/com/saboramazonico/activity/R.java
+++ gen/br/com/saboramazonico/activity/R.java
@@ -31,13 +31,21 @@ public final class R {
         public static final int pudim_tambaqui=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int imageView1=0x7f080000;
-        public static final int nome_receita=0x7f080001;
+        public static final int action_settings=0x7f080005;
+        public static final int button1=0x7f080000;
+        public static final int imageView1=0x7f080003;
+        public static final int nome_categoria=0x7f080001;
+        public static final int nome_receita=0x7f080004;
+        public static final int pager=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int receita_linha=0x7f030001;
+        public static final int categoria_linha=0x7f030001;
+        public static final int fragment_categorias=0x7f030002;
+        public static final int fragment_favoritos=0x7f030003;
+        public static final int fragment_home=0x7f030004;
+        public static final int menu_main=0x7f030005;
+        public static final int receita_linha=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
